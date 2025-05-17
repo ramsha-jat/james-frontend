cf3fc2b58049eb81cc7cda8b8eb5c1736c2bd979
ramsha-jat <ramsha.bscsf19@iba-suk.edu.pk>
2025-05-17 20:55:53 +0500

    aws added

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000", // Your backend URL
+  baseURL: "http://james1.twilightparadox.com", // Your backend URL
   headers: {
     "Content-Type": "application/json",
   },
